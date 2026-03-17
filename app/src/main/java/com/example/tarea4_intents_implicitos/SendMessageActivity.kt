e92cc4417c57f8f66e36829ac23e539b32bc01c0
ᓚᘏᗢ !? <datamayo.211@gmail.com>
2026-03-16 23:10:50 -0600

    Layout de activity terminado y configuración temprana de la pantalla de inicio

--- app/src/main/java/com/example/tarea4_intents_implicitos/SendMessageActivity.kt
+++ app/src/main/java/com/example/tarea4_intents_implicitos/SendMessageActivity.kt
@@ -3,37 +3,40 @@ package com.example.tarea4_intents_implicitos
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+class SendMessageActivity : AppCompatActivity() {
 
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var sendMessage : Button
-    private lateinit var takePicture : Button
+    private lateinit var textboxSend : EditText
+    private lateinit var btnSend : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_send_message)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        sendMessage = findViewById(R.id.send_message)
-        takePicture = findViewById(R.id.take_picture)
+        textboxSend = findViewById(R.id.textbox_send)
+        btnSend = findViewById(R.id.btn_send)
 
-        sendMessage.setOnClickListener { _ ->
-            val intent = Intent(this, SendMessageActivity::class.java)
-            startActivity(intent)
+        btnSend.setOnClickListener(){
+
         }
 
-        takePicture.setOnClickListener { _ ->
-            val intent = Intent(this, camera_activity::class.java)
-            startActivity(intent)
+        val sendText: Intent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, textboxSend.text)
+            type = "text/plain"
         }
+
+        val shareText = Intent.createChooser(sendText, null)
+        startActivity(shareText)
     }
 }
